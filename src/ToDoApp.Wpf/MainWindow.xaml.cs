1f0e64cca4e424af9679c0695517fdd41e3ceab5
Mitchell Wommack <Dwmitch84@gmail.com>
2020-02-20 11:29:29 -0800

    need to serialize items, not documents

--- src/ToDoApp.Wpf/MainWindow.xaml.cs
+++ src/ToDoApp.Wpf/MainWindow.xaml.cs
@@ -1,8 +1,6 @@
-﻿using System.Windows;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
-using System.Diagnostics;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
 using System.Windows.Controls;
 using ToDoApp.Wpf.Models;
 
@@ -15,13 +13,15 @@ namespace ToDoApp.Wpf
     {
         public MainWindow()
         {
+
             InitializeComponent();
-            
-
         }
-
-
-
+        private const string FilterTxt = "Text file (*.txt)|*.txt";
+        private const string FilterJson = "JSON file (*.json)|*.json";
+        private const string FilterXml = "XML file (*.xml)|*.xml";
+        private const string FilterSoap = "SOAP file (*.soap)|*.soap";
+        private const string FilterBinary = "Binary file (*.bin)|*.bin";
+        private const string FilterAny = "Any files (*.*)|*.*";
         private void OnAddTodoTaskButtonClick(object sender, RoutedEventArgs e)
         {
             TodoTask item = new TodoTask();
@@ -55,53 +55,276 @@ namespace ToDoApp.Wpf
 
         private void FileSave_Click(object sender, RoutedEventArgs e)
         {
-            string path = "output.txt";
-            if (System.IO.File.Exists(path))
+
+
+            Microsoft.Win32.SaveFileDialog dialog = new Microsoft.Win32.SaveFileDialog();
+
+            dialog.Filter = FilterTxt + "|" +
+                            FilterJson + "|" +
+                            FilterXml + "|" +
+                            FilterSoap + "|" +
+                            FilterBinary;
+
+            dialog.FilterIndex = 1;
+
+
+            bool? dialogResult = dialog.ShowDialog();
+            if (dialogResult == true)
             {
-                File.Delete(path); //deletes the old file, creating a new list
-
-                System.IO.FileStream fileStream = System.IO.File.Open(
-                path,
-                System.IO.FileMode.Append,
-                System.IO.FileAccess.Write,
-                System.IO.FileShare.None);
-                System.IO.StreamWriter writer = new System.IO.StreamWriter(fileStream);
-
-                foreach (var item in TodoTaskListView.Items)
+                string filePath = dialog.FileName;
+                if (System.IO.File.Exists(filePath))
                 {
-                    TodoTask listitem = item as TodoTask;
-                    writer.WriteLine(listitem.Description);
+                    System.IO.File.Delete(filePath);
                 }
 
-                writer.Close();
-                fileStream.Close();
+                // get content from UI control
+                string content = "";
 
-
-            }
-            else
-            {
-
-                System.IO.StreamWriter writer = new System.IO.StreamWriter(path);
-
-                foreach (var item in TodoTaskListView.Items)
+                // determine which format the user want to save as
+                if (dialog.FilterIndex == 1)
                 {
-                    TodoTask listitem = item as TodoTask;
-                    writer.WriteLine(listitem.Description); 
-                    //
+                    // write as txt
+                    System.IO.StringWriter writer = new System.IO.StringWriter();
+                    List<TodoTask> items = new List<TodoTask>();
+                    foreach (var item in this.TodoTaskListView.Items)
+                    {
+                        items.Add(item as TodoTask);
+                    }
+
+                    foreach (var item in items)
+                    {
+                        writer.WriteLine(item.Description);
+                    }
+                    /* writer.WriteLine(DateTime.Now);   // write last saved
+                     writer.Write(content);            // write content */
+                    content = writer.ToString();      // assign assembled content
+
+                    writer.Dispose();                 // release writer
+                }
+                else if (dialog.FilterIndex == 2)
+                {
+
+                    System.IO.StringWriter writer = new System.IO.StringWriter();
+                    List<TodoTask> items = new List<TodoTask>();
+                    foreach (var item in this.TodoTaskListView.Items)
+                    {
+                        items.Add(item as TodoTask);
+                    }
+
+                    foreach (var item in items)
+                    {
+                        writer.WriteLine(item.Description);
+                    }
+
+                    // write as JSON
+                    // create object to serialize
+                    Models.TodoTask document = new Models.TodoTask(content);
+                    // serialize type (Models.Document) to JSON string 
+                    string json = Newtonsoft
+                        .Json
+                        .JsonConvert
+                        .SerializeObject(document);
+                    // set content to JSON result
+                    content = json;                   // assign JSON string to content
+                }
+                else if (dialog.FilterIndex == 3)
+                {
+                    System.IO.StringWriter writer = new System.IO.StringWriter();
+                    List<TodoTask> items = new List<TodoTask>();
+                    foreach (var item in this.TodoTaskListView.Items)
+                    {
+                        items.Add(item as TodoTask);
+                    }
+
+                    foreach (var item in items)
+                    {
+                        writer.WriteLine(item.Description);
+                    }
+
+                    // write as XML
+                    // create object to serialize
+                    Models.TodoTask document = new Models.TodoTask(content);
+                    // create serializer
+                    System.Xml.Serialization.XmlSerializer serializer =
+                        new System.Xml.Serialization.XmlSerializer(typeof(Models.TodoTask));
+                    // this serializer writes to a stream
+                    System.IO.MemoryStream stream = new System.IO.MemoryStream();
+                    serializer.Serialize(stream, document);
+                    stream.Seek(0, System.IO.SeekOrigin.Begin);   // reset stream to start
+
+                    // read content from stream
+                    System.IO.StreamReader reader = new System.IO.StreamReader(stream);
+                    content = reader.ReadToEnd();   // assign XML string to content
+
+                    reader.Dispose();   // dispose the reader
+                    stream.Dispose();   // dispose the stream
+                }
+                else if (dialog.FilterIndex == 4)
+                {
+                    System.IO.StringWriter writer = new System.IO.StringWriter();
+                    List<TodoTask> items = new List<TodoTask>();
+                    foreach (var item in this.TodoTaskListView.Items)
+                    {
+                        items.Add(item as TodoTask);
+                    }
+
+                    foreach (var item in items)
+                    {
+                        writer.WriteLine(item.Description);
+                    }
+
+                    // write as SOAP
+                    // note: have to add a reference to a global assembly (dll) to use in project 
+                    // create object to serialize
+                    Models.TodoTask document = new Models.TodoTask(content);
+                    // create serializer
+                    /*System.Runtime.Serialization.Formatters.Soap.SoapFormatter serializer =
+                        new System.Runtime.Serialization.Formatters.Soap.SoapFormatter();
+                    // this serializer writes to a stream
+                    System.IO.MemoryStream stream = new System.IO.MemoryStream();
+                    serializer.Serialize(stream, document);
+                    stream.Seek(0, System.IO.SeekOrigin.Begin);   // reset stream to start
+
+                    // read content from stream
+                    System.IO.StreamReader reader = new System.IO.StreamReader(stream);
+                    content = reader.ReadToEnd();   // assign SOAP string to content
+
+                    reader.Dispose();   // dispose the reader
+                    stream.Dispose();   // dispose the stream
+*/
+                }
+                else // implies this is last one (binary)
+                {
+                    System.IO.StringWriter writer = new System.IO.StringWriter();
+                    List<TodoTask> items = new List<TodoTask>();
+                    foreach (var item in this.TodoTaskListView.Items)
+                    {
+                        items.Add(item as TodoTask);
+                    }
+
+                    foreach (var item in items)
+                    {
+                        writer.WriteLine(item.Description);
+                    }
+
+                    // write as binary
+                    // create object to serialize
+                    Models.TodoTask document = new Models.TodoTask(content);
+                    // create serializer
+                    System.Runtime.Serialization.Formatters.Binary.BinaryFormatter serializer =
+                        new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                    // this serializer writes to a stream
+                    System.IO.MemoryStream stream = new System.IO.MemoryStream();
+                    serializer.Serialize(stream, document);
+
+                    stream.Seek(0, System.IO.SeekOrigin.Begin);   // reset stream to start
+                    // reading and writing binary data directly as string has issues, try not to do it 
+                    content = Convert.ToBase64String(stream.ToArray());    // assign base64 to content
+
+                    stream.Dispose();   // dispose the stream
                 }
 
-                writer.Close();
-            }
+                // write content to file
+                System.IO.File.WriteAllText(filePath, content);
 
+                /* string path = "output.txt";
+                 if (System.IO.File.Exists(path))
+                 {
+                     File.Delete(path); //deletes the old file, creating a new list
+
+                     System.IO.FileStream fileStream = System.IO.File.Open(
+                     path,
+                     System.IO.FileMode.Append,
+                     System.IO.FileAccess.Write,
+                     System.IO.FileShare.None);
+                     System.IO.StreamWriter writer = new System.IO.StreamWriter(fileStream);
+
+                     foreach (var item in TodoTaskListView.Items)
+                     {
+                         TodoTask listitem = item as TodoTask;
+                         writer.WriteLine(listitem.Description);
+                     }
+
+                     writer.Close();
+                     fileStream.Close();
+
+
+                 }
+                 else
+                 {
+
+                     System.IO.StreamWriter writer = new System.IO.StreamWriter(path);
+
+                     foreach (var item in TodoTaskListView.Items)
+                     {
+                         TodoTask listitem = item as TodoTask;
+                         writer.WriteLine(listitem.Description); 
+                         //
+                     }
+
+                     writer.Close();
+                 }*/
+            }
 
         }
 
         private void FileOpen_Click(object sender, RoutedEventArgs e)
         {
-            string path = "output.txt";
+            //old code that worked with defaul file path
+            //string path = "output.txt";
 
-       
-            if (System.IO.File.Exists(path))
+            Microsoft.Win32.OpenFileDialog dialog = new Microsoft.Win32.OpenFileDialog();
+
+            dialog.Filter = FilterTxt + "|" +
+                            FilterJson + "|" +
+                            FilterXml + "|" +
+                            FilterSoap + "|" +
+                            FilterBinary;
+
+
+            dialog.FilterIndex = 1;
+            bool? dialogResult = dialog.ShowDialog();
+            if (dialogResult == true)
+            {
+
+                string filePath = dialog.FileName;
+                if (System.IO.File.Exists(filePath))
+                {
+                    string content = System.IO.File.ReadAllText(filePath);
+
+                    if (dialog.FilterIndex == 1)
+                    {
+
+
+                        System.IO.FileStream fileStream = System.IO.File.Open(
+               filePath,
+               System.IO.FileMode.Open,
+               System.IO.FileAccess.Read,
+               System.IO.FileShare.None);
+                        System.IO.StreamReader reader = new System.IO.StreamReader(fileStream);
+                        while (reader.EndOfStream == false)
+                        {
+                            string line = reader.ReadLine();
+                            TodoTask item = new TodoTask();
+                            item.Description = line;
+                            TodoTaskListView.Items.Add(item);
+                        }
+                        reader.Close();
+                        fileStream.Close();
+
+
+                    }
+
+                    else
+                    {
+
+                    }
+                }
+
+            }
+
+            //old code that worked with defaul file path
+            /*if (System.IO.File.Exists(path))
             {
                 System.IO.FileStream fileStream = System.IO.File.Open(
                 path,
@@ -119,11 +342,26 @@ namespace ToDoApp.Wpf
                 reader.Close();
                 fileStream.Close();
 
+            }*/
+
+            //below is part of the save file code
+            /*System.IO.StringWriter writer = new System.IO.StringWriter();
+            List<TodoTask> items = new List<TodoTask>();
+            foreach (var item in this.TodoTaskListView.Items)
+            {
+                items.Add(item as TodoTask);
             }
 
-
-
+            foreach (var item in items)
+            {
+                writer.WriteLine(item.Description);
+            }*/
         }
+
     }
+
+
+
 }
-    
+
+
--- src/ToDoApp.Wpf/Models/TodoTask.cs
+++ src/ToDoApp.Wpf/Models/TodoTask.cs
@@ -1,11 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ToDoApp.Wpf.Models
+﻿namespace ToDoApp.Wpf.Models
 {
-    class TodoTask
+    /// <summary>
+    /// Represents a document
+    /// </summary>
+    [System.Serializable]
+    public class TodoTask
     {
         public string Description { get; set; }
+
+        // NOTE: to use binary or soap formatting for serialization the [Serializable] attribute is required
+
+        /// <summary>
+        /// Gets, sets the content of the document
+        /// </summary>
+        public string Content { get; set; }
+
+        /// <summary>
+        /// Gets, sets the date and time last saved
+        /// </summary>
+        public System.DateTime LastSaved { get; set; }
+
+        /// <summary>
+        /// Default, parameter-less constructor
+        /// </summary>
+        public TodoTask()
+        {
+            Content = string.Empty;
+            LastSaved = System.DateTime.Now;
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="content">Content for instance</param>
+        public TodoTask(string content)
+        {
+            Content = content;
+            LastSaved = System.DateTime.Now;
+        }
     }
 }
